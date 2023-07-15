40d336e9306b05335f16428f39c559ba1ec03329
Baby B <85929770+Baby3370@users.noreply.github.com>
2023-07-15 07:05:59 +0530

    Create lowertri.c
    to calculate sum of lower triangular matrix

---
+++ lowertri.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+   int i, j, m, n, sum = 0;
+   
+   printf("Enter the number of rows: ");
+   scanf("%d", &m);
+   
+   printf("Enter the number of columns: ");
+   scanf("%d", &n);
+   
+   int a[m][n];
+   
+   printf("Enter the elements:\n");
+   for (i = 0; i < m; i++) {
+       for (j = 0; j < n; j++) {
+           scanf("%d", &a[i][j]);
+       }
+   }
+   
+   printf("The elements:\n");
+   for (i = 0; i < m; i++) {
+       for (j = 0; j < n; j++) {
+           printf("%d\t", a[i][j]);
+       }
+       printf("\n");  
+   }
+   
+   
+   for (i = 0; i < m; i++) {
+       for (j = 0; j <= i; j++) {
+           sum += a[i][j];
+       }
+   }
+   
+   printf("Sum of lower triangular matrix: %d\n", sum);
+   
+   return 0;
+}
